61d6a602df4f576d79da2949dd0cfd87b4a5aab0
ManuelGalindez <manuelgalindez81@gmail.com>
2018-01-11 22:46:26 +0545

    Agg reducer, store, actions

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const $form = document.getElementById('form');
 
 $form.addEventListener('submit', handelSubmit)
@@ -7,4 +9,55 @@ function handelSubmit(ev) {
   const data = new FormData($form);
   const title = data.get('title');
   console.log(title);
+  store.dispatch({
+    type: 'ADD_SONG',
+    payload: {
+      title
+    },
+  });
 }
+
+const initialState = [
+  {
+    title: 'Despacito'
+  },
+  {
+    title: 'One more time'
+  },
+  {
+    title: 'Titanium'
+  },
+];
+
+const reducer = function(state, action) {
+  switch (action.type) {
+    case 'ADD_SONG':
+      return [...state, action.payload]
+    default:
+      return state
+  }
+}
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const $container = document.getElementById('playlist');
+
+function render() {
+  const playlist = store.getState();
+  const template = `
+    <ul>
+      ${playlist.map(song => `
+        <li>${song.title}</li>
+      `
+    )}
+    </ul>
+  `;
+  $container.innerHTML = template
+}
+render();
+
+store.subscribe(render);
