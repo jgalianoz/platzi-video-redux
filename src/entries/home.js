ed14c76a86f6acb43463c31af2bb5944feb6d4bf
ManuelGalindez <manuelgalindez81@gmail.com>
2018-01-12 12:24:17 +0545

    [add] store platzi-video-app

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
+import { createStore } from 'redux';
+
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
 import data from '../api.json';
-// console.log('Hola mundo!' )
+
+const initialState = {
+  data: data
+}
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState());
 
 const homeContainer = document.getElementById('home-container')
 
 // ReactDOM.render(que voy a renderizar, donde lo haré);
 // const holaMundo = <h1>hola Estudiante!</h1>;
 hydrate( <Home data={data} />, homeContainer);
-
