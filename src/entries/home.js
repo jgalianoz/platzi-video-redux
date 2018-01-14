12b403208f07b896fc2ea419d368ba5a645998fd
ManuelGalindez <manuelgalindez81@gmail.com>
2018-01-14 20:25:00 +0545

    refactor platzi-video

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
@@ -22,4 +23,10 @@ const homeContainer = document.getElementById('home-container')
 
 // ReactDOM.render(que voy a renderizar, donde lo haré);
 // const holaMundo = <h1>hola Estudiante!</h1>;
-hydrate( <Home data={data} />, homeContainer);
+
+hydrate(
+  <Provider store={store}>
+    <Home />
+  </Provider>,
+  homeContainer
+);
