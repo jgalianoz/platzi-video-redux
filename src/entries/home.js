ad9ac77b8c0746fd16f83ed658e6dab9707baaf3
ManuelGalindez <manuelgalindez81@gmail.com>
2018-01-15 20:15:03 +0545

    [add] reducer platzi-video

--- src/entries/home.js
+++ src/entries/home.js
@@ -7,12 +7,14 @@ import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
 import data from '../api.json';
 
+import reducer from '../reducers/data'
+
 const initialState = {
   data: data
 }
 
 const store = createStore(
-  (state) => state,
+  reducer,
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
---
+++ src/reducers/data.js
@@ -0,0 +1,9 @@
+export default function data(state, action) {
+  switch (action.type) {
+    case 'ADD': {
+      return state
+    }
+    default:
+      return state
+  }
+}
